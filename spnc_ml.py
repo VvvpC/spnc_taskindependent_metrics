dfb3beffffefa3d63baa49225a873c8b6c0827f2
Chen_S <zchen153@sheffield.ac.uk>
2025-08-18 08:56:31 +0100

    Update spnc_ml.py

--- spnc_ml.py
+++ spnc_ml.py
@@ -93,7 +93,7 @@ def spnc_narma10(Ntrain,Ntest,Nvirt,m0, bias,
     y_test = d[Ntrain:]
     
     # print first 10 elements of x_train and y_train
-    print("first 10 elements of x_train: ", x_train[:10])
+    # print("first 10 elements of x_train: ", x_train[:10])
 
 
     # print("Samples for training: ", len(x_train))
@@ -564,9 +564,9 @@ def spnc_TI46(speakers, Nvirt, m0, bias=True, res_transform = None, params = Non
         pre_process = mfcc(rate=train_rate[0], nfft=1024)
 
     x_train = pre_process.fit_transform(train_signal)
-    print('the shape of x_train is: ', x_train.shape)
-    print('the feature of the first utterance is: ', x_train[0])
-    print('the shape of the first utterance is: ', x_train[0].shape)
+    # print('the shape of x_train is: ', x_train.shape)
+    # print('the feature of the first utterance is: ', x_train[0])
+    # print('the shape of the first utterance is: ', x_train[0].shape)
 
 
     #Normalise the input into the range 0 - 1
@@ -574,14 +574,16 @@ def spnc_TI46(speakers, Nvirt, m0, bias=True, res_transform = None, params = Non
     # so we need to normalise the input into the range 0 - 1
     prescaler = normaliser()
     xn = prescaler.fit_transform(x_train)
-    print('the shape of xn is: ', xn.shape)
-    print('after normalise, the first feature value is: ', xn[0][0])
+    # print('the type of xn is: ', type(xn))
+    # print('the shape of xn is: ', xn.shape)
+    # print('after normalise, the first feature value is: ', xn[0][0])
 
 
     Nin = x_train[0].shape[-1]
     Nout = len(np.unique(train_label))
 
     print( 'Nin =', Nin, ', Nout = ', Nout, ', Nvirt = ', Nvirt)
+
 
 
     SNR = single_node_reservoir(Nin, Nout, Nvirt, m0, dilution=1.0, res=res_transform)
@@ -594,8 +596,8 @@ def spnc_TI46(speakers, Nvirt, m0, bias=True, res_transform = None, params = Non
 
     S_train, J_train = SNR.transform(xn, params)
 
-    print(J_train.shape, J_train[0].shape)
-    print(S_train.shape, S_train[0].shape)
+    # print('J_train.shape, J_train[0].shape  ', J_train.shape, J_train[0].shape)
+    # print('S_train.shape, S_train[0].shape', S_train.shape, S_train[0].shape)
 
     Nblocks = 8
 
@@ -614,14 +616,14 @@ def spnc_TI46(speakers, Nvirt, m0, bias=True, res_transform = None, params = Non
     # act and inv_act are the activation function and it's inverse
     # either leave blank or set to linear to not apply activation fn
     net = linear(Nvirt, Nout, bias=bias)
-    print('net',net)
+    # print('net',net)
 
 
     # Select how many utterances to train on
     Ntrain_utter = 8
     split1, split2 = stratified_split((train_speaker, train_label), Ntrain_utter)
-    print('split1',split1)
-    print('split2',split2)
+    # print('split1',split1)
+    # print('split2',split2)
 
 
     # Create desired 1 hot output for the training
@@ -630,10 +632,10 @@ def spnc_TI46(speakers, Nvirt, m0, bias=True, res_transform = None, params = Non
     # Since TI46 is stored as a list of np arrays stack these into a flat array
     z_train_flat = np.vstack(z_train[split1])
     y_train_1h_flat = np.vstack(y_train_1h[split1])
-    print('the shape of z_train_flat is: ', z_train_flat.shape)
-    print('the shape of y_train_1h_flat is: ', y_train_1h_flat.shape)
-    print('the first element of z_train_flat is: ', z_train_flat[0])
-    print('the first element of y_train_1h_flat is: ', y_train_1h_flat[0])
+    # print('the shape of z_train_flat is: ', z_train_flat.shape)
+    # print('the shape of y_train_1h_flat is: ', y_train_1h_flat.shape)
+    # print('the first element of z_train_flat is: ', z_train_flat[0])
+    # print('the first element of y_train_1h_flat is: ', y_train_1h_flat[0])
 
     # Use the ridge regression training routine
     alpha = RR.Kfold_train(net, z_train_flat, y_train_1h_flat, 5, quiet=True)
@@ -666,6 +668,181 @@ def spnc_TI46(speakers, Nvirt, m0, bias=True, res_transform = None, params = Non
     x_test = pre_process.transform(test_signal)
     xn_test = prescaler.transform(x_test)
     S_test, J_test = SNR.transform(xn_test, params, force_compute)
+    z_test = post_process(S_test, Nblocks, plot=False)
+
+    # Predict the labels from the predicted output
+    pred_labels = np.array([ np.argmax(np.mean(net.forward(zi), axis=0)) for zi in z_test ])
+
+    print(classification_report(test_label, pred_labels, digits=3))
+
+    test_report = classification_report(test_label, pred_labels, output_dict=True)
+    conf_mat = confusion_matrix(test_label, pred_labels)
+    
+
+
+    return accuracy_score(test_label, pred_labels)
+
+
+
+def spnc_TI46_test(speakers, Nvirt, m0, bias=True, res_transform = None, params = None, prepro = "mfcc", *args, **kwargs):
+
+    # Select whether to run or load reservoir transformation from file
+    force_compute = True
+
+    # Specifying digits_only=True and train=True returns only the spoken digits part of TI20 training set
+    # It returns the signal, label, sampling rate and speaker of the data
+    train_signal, train_label, train_rate, train_speaker = TI46.load_TI20(speakers, digits_only=True, train=True)
+
+
+    # To load the test data, specify train=False
+    test_signal, test_label, test_rate, test_speaker = TI46.load_TI20(speakers, digits_only=True, train=False)
+
+    print("Samples for training: ", len(train_signal))
+    print("Samples for test: ", len(test_signal))
+
+
+
+    # Pre-processing, input is a list of utterances, and output is a list containg the mfcc features of each utterance
+    if prepro == "mfcc":
+        print('Using MFCC preprocessing')
+        pre_process = mfcc(rate=train_rate[0], nfft=1024)
+
+    x_train = pre_process.fit_transform(train_signal)
+    # print('the shape of x_train is: ', x_train.shape)
+    # print('the feature of the first utterance is: ', x_train[0])
+    # print('the shape of the first utterance is: ', x_train[0].shape)
+
+
+    #Normalise the input into the range 0 - 1
+    # becuse the mfcc features might have different value range between different dimensions, example: the value range is [0,1] for the first dimension, and [0,100] for the second dimension
+    # so we need to normalise the input into the range 0 - 1
+    prescaler = normaliser()
+    xn = prescaler.fit_transform(x_train)
+    print('the type of xn is: ', type(xn))
+    print('the shape of xn is: ', xn.shape)
+    print('after normalise, the first feature value is: ', xn[0][0])
+
+
+    Nin = x_train[0].shape[-1]
+    Nout = len(np.unique(train_label))
+
+    print( 'Nin =', Nin, ', Nout = ', Nout, ', Nvirt = ', Nvirt)
+
+    dataset_size = xn.shape[0]
+    S_train = np.empty(dataset_size, dtype=object)
+    J_train = np.empty(dataset_size, dtype=object)
+
+    # 将xn拆分依次进入reservoir，得到S_train
+    for i in range(xn.shape[0]):
+        single_xn = xn[i]
+        print('the type of single_xn is: ', type(single_xn))
+        print('the shape of single_xn is: ', single_xn.shape)
+        print('the first element of single_xn is: ', single_xn[0])
+
+        SNR = single_node_reservoir(Nin, Nout, Nvirt, m0, dilution=1.0, res=res_transform)
+
+        fixed_mask = kwargs.get('fixed_mask', True)
+        if fixed_mask: # 这里有个问题：没有注明seed。
+            print("Deterministic mask will be used")
+            SNR.M = fixed_seed_mask(Nin, Nvirt, m0)
+            # print('the shape of the mask is: ', SNR.M.M.shape)
+
+        S, J = SNR.transform(single_xn, params)
+        S_train[i] = S
+        J_train[i] = J
+
+    print('the type of S_train is: ', type(S_train))
+    print('J_train.shape, J_train[0].shape  ', J_train.shape, J_train[0].shape)
+    print('S_train.shape, S_train[0].shape', S_train.shape, S_train[0].shape)
+
+    Nblocks = 8
+
+    res_scaler = scaler()
+
+    res_scaler.fit(S_train)
+
+    #post_process = lambda S, *args, **kwargs : np.copy(S)
+    #post_process = lambda S, *args, **kwargs : res_scaler.transform(block_process(S, Nblocks, plot=True))
+    post_process = lambda S, *args, **kwargs : res_scaler.transform(S)
+
+    z_train = post_process(S_train, Nblocks, plot=True)
+
+
+    # Instantiate a linear output layer
+    # act and inv_act are the activation function and it's inverse
+    # either leave blank or set to linear to not apply activation fn
+    net = linear(Nvirt, Nout, bias=bias)
+    # print('net',net)
+
+
+    # Select how many utterances to train on
+    Ntrain_utter = 8
+    split1, split2 = stratified_split((train_speaker, train_label), Ntrain_utter)
+    # print('split1',split1)
+    # print('split2',split2)
+
+
+    # Create desired 1 hot output for the training
+    y_train_1h = create_1hot_like(Nout, z_train, train_label)
+
+    # Since TI46 is stored as a list of np arrays stack these into a flat array
+    z_train_flat = np.vstack(z_train[split1])
+    y_train_1h_flat = np.vstack(y_train_1h[split1])
+    # print('the shape of z_train_flat is: ', z_train_flat.shape)
+    # print('the shape of y_train_1h_flat is: ', y_train_1h_flat.shape)
+    # print('the first element of z_train_flat is: ', z_train_flat[0])
+    # print('the first element of y_train_1h_flat is: ', y_train_1h_flat[0])
+
+    # Use the ridge regression training routine
+    alpha = RR.Kfold_train(net, z_train_flat, y_train_1h_flat, 5, quiet=True)
+    print('Optimal regression parameter = ', alpha)
+
+    #Save the weights if needed
+    # np.savetxt('Weights', net.W)
+
+
+    # Calculated the predicted labels on the training set and print information
+    print('Train report')
+    pred_labels = np.array([ np.argmax(np.mean(net.forward(zi), axis=0)) for zi in z_train[split1] ])
+    print(classification_report(train_label[split1], pred_labels, digits=3))
+
+    # If some utterances were held back for validation now calculate predicted labels
+    if Ntrain_utter < 10:
+        print('Valid report')
+        pred_labels = np.array([ np.argmax(np.mean(net.forward(zi), axis=0)) for zi in z_train[split2] ])
+        print(classification_report(train_label[split2], pred_labels, digits=3))
+
+        valid_report = classification_report(train_label[split2], pred_labels, output_dict=True)
+        conf_mat = confusion_matrix(train_label[split2], pred_labels)
+
+
+    # Now perform calculation on the test part of the data set
+
+    params["name"] = "test"
+
+    # Use the fitted/trained parts of the model to transform the test data
+    x_test = pre_process.transform(test_signal)
+    xn_test = prescaler.transform(x_test)
+
+    # 将xn_test拆分依次进入reservoir，得到S_test
+    dataset_size = xn_test.shape[0]
+    S_test = np.empty(dataset_size, dtype=object)
+    J_test = np.empty(dataset_size, dtype=object)
+
+    for i in range(xn_test.shape[0]):
+        single_xn_test = xn_test[i]
+        SNR = single_node_reservoir(Nin, Nout, Nvirt, m0, dilution=1.0, res=res_transform)
+
+        fixed_mask = kwargs.get('fixed_mask', True)
+        if fixed_mask: # 这里有个问题：没有注明seed。
+            print("Deterministic mask will be used")
+            SNR.M = fixed_seed_mask(Nin, Nvirt, m0)
+            # print('the shape of the mask is: ', SNR.M.M.shape)
+
+        S, J = SNR.transform(single_xn_test, params)
+        S_test[i] = S
+        J_test[i] = J
+
     z_test = post_process(S_test, Nblocks, plot=False)
 
     # Predict the labels from the predicted output
